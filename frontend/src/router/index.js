2c01a2c50dbda78563ddc7f9c4abe4fbbc6f0c4b
Moch. Raditya Wiryateja <radityawiryateja62@gmail.com>
2026-07-06 09:11:47 +0700

    Change router history mode to hash

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { isAuthenticated } from '../lib/auth'
 import LoginPage from '../components/LoginPage.vue'
 import TelegramCallback from '../components/TelegramCallback.vue'
