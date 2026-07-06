c38ffbd20f3c384ebb578f723e35f07ee8482886
Moch. Raditya Wiryateja <radityawiryateja62@gmail.com>
2026-07-06 07:06:38 +0700

    Add Telegram OIDC configuration variables

--- backend/app/config.py
+++ backend/app/config.py
@@ -11,6 +11,9 @@ class Settings:
     SESSION_SECRET: str = os.getenv("SESSION_SECRET", "change-me")
     SESSION_EXPIRE_MINUTES: int = int(os.getenv("SESSION_EXPIRE_MINUTES", "10080"))
     FRONTEND_ORIGIN: str = os.getenv("FRONTEND_ORIGIN", "http://localhost:5173")
+    TELEGRAM_OIDC_CLIENT_ID: str = os.getenv("TELEGRAM_OIDC_CLIENT_ID", "")
+    TELEGRAM_OIDC_CLIENT_SECRET: str = os.getenv("TELEGRAM_OIDC_CLIENT_SECRET", "")
+    TELEGRAM_OIDC_REDIRECT_URI: str = os.getenv("TELEGRAM_OIDC_REDIRECT_URI", "")
 
 
 settings = Settings()
